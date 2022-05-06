9644a5aed5f7c87adcb717d0b3a51d70b55b6315
Santiago Sanmartín <saureth001@gmail.com>
2022-05-05 21:02:02 -0500

    [REF] Using formik components and Yup validations

--- src/components/screens/auth/Login.js
+++ src/components/screens/auth/Login.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import styled from 'styled-components';
 import { device } from '../../shared/media-devices/devices';
+import { Formik, Form } from 'formik';
+import regex from '../../shared/regex/regex';
+import EmailInput from '../../shared/shared-comps/EmailInput';
+import Label from '../../shared/shared-comps/Label';
+import  * as Yup from 'yup';
+import PasswordInput from '../../shared/shared-comps/PasswordInput';
 
 const Container = styled.div`
     display: flex;
@@ -18,61 +24,6 @@ const Container = styled.div`
 
 const Welcome = styled.div`
     font-size: 32px;
-`;
-
-const Label= styled.label`
-    font-size: 20px;
-`;
-
-const Input = styled.input.attrs(props => ({
-    type: 'text',
-    placeholder: 'email@test.co',
-}))
-`
-    outline: 0;
-    border-width: 0 0 2px;
-    border-color: #0F8DA9;
-    background-color: transparent;
-    font-size: 16px;
-    color: #173A8C;
-    :focus {
-        border-color: #173A8C;
-    }
-    ::placeholder {
-        color: #84A3E8;
-        padding-left: 5px;
-    }
-    width: 100%;
-    @media ${device.mobileL}{
-        min-width: 250px;
-        width: 50%;
-    }
-`;
-
-const Password = styled.input.attrs({
-    type: 'password',
-    placeholder: '******'
-})`
-    outline: 0;
-    border-width: 0 0 2px;
-    border-color: #0F8DA9;
-    background-color: transparent;
-    width: 50%;
-    min-width: 250px;
-    font-size: 16px;
-    color: #173A8C;
-    :focus {
-        border-color: #173A8C;
-    }
-    ::placeholder {
-        color: #84A3E8;
-        padding-left: 5px;
-    }
-    width: 100%;
-    @media ${device.mobileL}{
-        min-width: 250px;
-        width: 50%;
-    }
 `;
 
 const Button = styled.button`
@@ -93,19 +44,32 @@ const Button = styled.button`
 
 const Login = () => {
     return (
-        <Container>
-            <Welcome>Welcome!</Welcome>
-            <br />
-            <Label> Please log in to continue </Label>
-            <br />
-            <Label> Email </Label>
-            <Input />
-            <br />
-            <Label> Password </Label>
-            <Password />
-            <br />
-            <Button > SIGN IN </Button>
-        </Container>
+    <Formik
+        initialValues={{ email: '', password: '',}}
+        onSubmit={values => console.log(values)}
+        validationSchema={Yup.object({
+            email: Yup.string()
+                .required('Obligatorio')
+                .matches(regex,'Formato de correo incorrecto'),
+            password: Yup.string()
+                .required('Obligatorio')
+          }
+        )}
+    >            
+        <Form>
+            <Container>
+                <Welcome>Welcome!</Welcome>
+                <br />
+                <Label> Please log in to continue </Label>
+                <br />
+                <EmailInput name='email' label= "Email" />
+                <br />
+                <PasswordInput name='password' label= "Password"/>
+                <br />
+                <Button type='submit'> SIGN IN </Button>
+            </Container>
+        </Form>
+    </Formik>
     );
 }
 
